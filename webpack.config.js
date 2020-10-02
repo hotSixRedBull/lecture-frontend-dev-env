8239108b6fcb1fe8a1a126d90dbaccd28eda0360
Alan Seunggi Lee <software.lee@samsung.com>
2020-10-02 13:56:19 +0900

    proto

--- webpack.config.js
+++ webpack.config.js
@@ -34,10 +34,16 @@ module.exports = {
           name: "[name].[ext]?[hash]",
           limit: 10000 // 10Kb
         }
-      }
+      },
       /**
        * TODO: babel-loader를 구성해 보세요.
        */
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader'
+        ]
+      }
     ]
   },
   plugins: [
