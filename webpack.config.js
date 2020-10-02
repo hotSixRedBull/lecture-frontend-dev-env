5c53b1ec01d82f7fab6131aa5619bedc8c8af691
Alan Seunggi Lee <software.lee@samsung.com>
2020-10-02 11:29:06 +0900

    loader

--- webpack.config.js
+++ webpack.config.js
@@ -14,8 +14,8 @@ module.exports = {
           {
               test:/\.css$/,
               use: [
-                'css-loader',
-                'style-loader'
+                'style-loader',
+                'css-loader'
               ]
           },
           {
