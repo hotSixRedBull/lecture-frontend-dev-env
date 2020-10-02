f76c89cfa5321110da5dc65cfa0ae6a5964a8110
Alan Seunggi Lee <software.lee@samsung.com>
2020-10-02 10:14:27 +0900

    failed

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,27 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
+  },
+  module: {
+      rules: [
+          {
+              test:/\.css$/,
+              use: [
+                'css-loader',
+                'style-loader'
+              ]
+          },
+          {
+              test:/\.(png|jpg)$/,
+              use: {
+                  loader: 'file-loader',
+                  options: {
+                      name: '[name].[ext]?[hash]',
+                      publicPath: './dist/',
+                      limit: 200000
+                  }
+              }
+          }
+      ]
   }
 }
